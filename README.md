290cdd71fbd97e215c81482b3e75a7a69e4eb543
Keijia <antonyblack05@gmail.com>
2021-03-20 00:01:00 +0300

    serverssss

--- README.md
+++ README.md
@@ -3,8 +3,14 @@ The Ultimate Osu Server Launcher is a server
 launcher that allows you to switch between variety of osu! servers. 
 
 # Current servers
+- [Banchi](https://osu.ppy.sh)
 - [Gatari](https://osu.gatari.pw)
 - [Kurikku](https://kurikku.pw)
 - [Akatsuki](https://akatsuki.pw)
+- [EZPPFarm](https://ez-pp.farm)
+- [RealistikOsu](https://ussr.pl)
+- [Astellia](https://astellia.club)
 - [Shizofrenia](https://osu.shizofrenia.pw)
+- [Debian](https://debian.moe)
+- [Horizon](https://lemres.de)
 - [Iteki](https://iteki.pw)
--- UltimateOsuServerLauncher/Models/Config.cs
+++ UltimateOsuServerLauncher/Models/Config.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Net;
 using Newtonsoft.Json;
 using UltimateOsuServerLauncher.Utils;
@@ -28,10 +29,16 @@ namespace UltimateOsuServerLauncher.Models
             OsuPath = string.Empty;
             Servers = new[]
             {
+                new Server("Bancho", "osu.py.sh", "https://osu.ppy.sh", "ppy.sh"),
                 new Server("Gatari", "osu.gatari.pw", "https://osu.gatari.pw", "gatari.pw"),
                 new Server("Kurikku", "kurikku.pw", "https://kurikku.pw", "kurikku.pw"),
                 new Server("Akatsuki", "akatsuki.pw", "https://akatsuki.pw", "akatsuki.pw"),
+                new Server("EZPPFarm", "ez-pp.farm", "https://ez-pp.farm", "ez-pp.farm"),
+                new Server("RealistikOsu", "ussr.pl", "https://ussr.pl", "ussr.pl"),
+                new Server("Astellia", "astellia.club", "https://astellia.club", "astellia.club"),
                 new Server("Shizofrenia", "osu.shizofrenia.pw", "https://osu.shizofrenia.pw", "shizofrenia.pw"),
+                new Server("Debian", "debian.moe", "https://debian.moe", "debian.moe"),
+                new Server("Horizon", "lemres.de", "https://elemres.de", "lemres.de"),
                 new Server("Iteki", "iteki.pw", "https://iteki.pw", "iteki.pw")
             };
         }
